de0f7803c48bcb4664f11294e232f66a985885a9
cduployez <60548405+cduployez@users.noreply.github.com>
2020-02-28 22:33:33 +0100

    App Structure

--- src/App.svelte
+++ src/App.svelte
@@ -1,38 +1,104 @@
 <script>
-    import Nested from "./Nested.svelte";
+    import {onMount} from 'svelte';
 
-    const name = 'World';
+    function addBodyClass(className) {
+        if (className) {
+            window.document.body.classList.add(className);
+        }
+    }
+
+    function removeBodyClass(className) {
+        if (className) {
+            window.document.body.classList.remove(className);
+        }
+    }
+
+    function onThemeChange(themeInfo) {
+        if (themeInfo) {
+            removeBodyClass(themeInfo.previousTheme);
+            addBodyClass(themeInfo.currentTheme);
+        }
+    }
+
+    onMount(async () => {
+        // TODO Subscribe to theme change
+        // TODO Get current theme
+        onThemeChange({previousTheme: null, currentTheme: 'light-theme'/*themeStore.theme*/});
+    });
 </script>
 
-<style>
+<style type="text/scss">
+    @import "src/assets/styles/variables.scss";
 
+    main {
+        margin: 0;
+        padding: 0 10% 5rem;
+
+        .columns {
+            display: flex;
+            flex-flow: row-reverse wrap;
+            justify-content: space-between;
+
+            div {
+                width: calc(50vw - 15%);
+            }
+        }
+
+        CvFooter {
+            display: none;
+        }
+    }
+
+    @media (max-width: $tablet-width) {
+        main {
+            padding: 0 5% 5rem;
+
+            .columns {
+                div {
+                    width: 100%;
+                }
+            }
+
+            CvFooter {
+                display: block;
+            }
+        }
+    }
+
+    @media (max-width: $phone-width) {
+        main {
+            padding: 0 2% 5rem;
+        }
+    }
 </style>
 
-<!--<CvMenuBar/>-->
 <h1>Work in progress</h1>
+<!--
+
+&lt;!&ndash;<CvMenuBar/>&ndash;&gt;
 
 <main>
-    <!--<CvHeader/>-->
+    &lt;!&ndash;<CvHeader/>&ndash;&gt;
 
-    <!--<CvBanner/>-->
+    &lt;!&ndash;<CvBanner/>&ndash;&gt;
 
     <div class="columns">
         <div>
-            <!--<CvExperiences/>-->
-            <!--<CvProjects/>-->
+            &lt;!&ndash;<CvExperiences/>&ndash;&gt;
+            &lt;!&ndash;<CvProjects/>&ndash;&gt;
         </div>
 
         <div>
-            <!--<CvFormations/>-->
-            <!--<CvSkills/>-->
-            <!--<CvLanguages/>-->
-            <!--<CvHobbies/>-->
+            &lt;!&ndash;<CvFormations/>&ndash;&gt;
+            &lt;!&ndash;<CvSkills/>&ndash;&gt;
+            &lt;!&ndash;<CvLanguages/>&ndash;&gt;
+            &lt;!&ndash;<CvHobbies/>&ndash;&gt;
         </div>
 
     </div>
 
-    <!--<CvFooter/>-->
-</main>
+    &lt;!&ndash;<CvFooter/>&ndash;&gt;
+</main>-->
 
 
 
